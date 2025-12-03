bb329ea233f938c71ca147f7b781e43b73dfeb3d
stefa <stefan.j.hamm@gmail.com>
2025-12-03 19:16:32 +0100

    Add new agent, environment, and fileLoader modules

---
+++ src/agent.py
@@ -0,0 +1,8 @@
+
+
+
+#-Chooses actions based on observations
+#-Implements movement strategy
+#-Tracks personal state
+
+class Agent:
---
+++ src/environment.py
@@ -0,0 +1,9 @@
+
+
+#Floor plan, SFF, occupancy map
+#Physics/collision checking
+#Timestep advancement
+
+
+class Environment:
+    def __init__
